bd9dac32f2ee41b85abdb42fdcc4710570828d22
hs7kang <hs7kang@outlook.kr>
2017-04-04 22:01:24 +0900

    Add files via upload

---
+++ BitTest.java
@@ -0,0 +1,14 @@
+public class BitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Integer.toBinaryString(30));
+		System.out.println(Integer.toBinaryString(-1));
+		System.out.println(Integer.toBinaryString(30 & -1));
+		System.out.println(30 & -1);
+		
+		//테스트입니다.
+		System.out.println("테스트입니다");
+	}
+
+}
